a1e37658366f207ab742c29197bd7d40e298c30a
taddeoneri <taddeo.neri10@gmail.com>
2023-05-24 15:09:36 +0200

    migration

--- database/migrations/2023_05_24_125547_create_trains_table.php
+++ database/migrations/2023_05_24_125547_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company', 50);
+            $table->string('departure_station', 50);
+            $table->string('arrival_station', 50);
+            $table->time('departure time');
+            $table->time('arrival_time');
+            $table->string('train_code', 50);
+            $table->tinyInteger('carriages');
+            $table->boolean('delay');
+            $table->boolean('deleted');
             $table->timestamps();
         });
     }
